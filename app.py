ece0bdbd6f52b9c13c3cdb2d7f50385ca4821fd5
Mumu-mama <1801858057@qq.com>
2026-03-26 19:56:32 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ if st.button("点击我揣测璐璐心思"):
     if mood_index >= 60:
         st.success("王者之神降临，今晚周雯璐带领你打游戏！")
     else:
-        st.info("nono 可恶是连跪的气息，为了小命还是不打了0(ㄒoㄒ)0~~")
+        st.info("nono 可恶是连跪的气息，为了小命还是不打了\n0(ㄒoㄒ)0~~")
